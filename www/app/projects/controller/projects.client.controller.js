38d6cfde5064cf3fa101b021372eb19f0c8916ab
Amy Ly <amyly@ufl.edu>
2017-04-09 01:41:20 -0400

    test QR scanner

--- www/app/projects/controller/projects.client.controller.js
+++ www/app/projects/controller/projects.client.controller.js
@@ -9,14 +9,30 @@ angular.module('starter.projects', ['ionic', 'ngCordova', 'starter.config', 'use
 
 .controller('QRCtrl', function($scope, $cordovaBarcodeScanner) {
   $scope.scanBarcode = function() {
-    $cordovaBarcodeScanner.scan().then(function(imageData) {
-        alert(imageData.text);
-        console.log("Barcode Format -> " + imageData.format);
-        console.log("Cancelled -> " + imageData.cancelled);
-    }, function(error) {
-        console.log("An error happened -> " + error);
-    });
-  };
+    $cordovaBarcodeScanner.scan(
+      function (result) {
+        alert("We got a barcode\n" +
+          "Result: " + result.text + "\n" +
+          "Format: " + result.format + "\n" +
+          "Cancelled: " + result.cancelled);
+      },
+      function (error) {
+        alert("Scanning failed: " + error);
+      },
+      {
+        preferFrontCamera: true, // iOS and Android
+        showFlipCameraButton: true, // iOS and Android
+        showTorchButton: true, // iOS and Android
+        torchOn: true, // Android, launch with the torch switched on (if available)
+        prompt: "Place a barcode inside the scan area", // Android
+        resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+        formats: "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+        orientation: "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+        disableAnimations: true, // iOS
+        disableSuccessBeep: false // iOS
+      }
+    );
+  }
 })
 
 .controller('ProjectsController', function($scope, $http, $stateParams, $location, $rootScope, $window) {
@@ -195,4 +211,4 @@ angular.module('starter.projects', ['ionic', 'ngCordova', 'starter.config', 'use
 
       return array;
     }
-});
+});;
